c7e12f0c8c1384e120c36e4ed98e3b64a0f899a3
Mark Siazon <markangelosiazon@gmail.com>
2025-01-29 09:01:33 +0800

    🔨 | Update configs + Comments & GH Pages script

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"; // New change from TailwindV4
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/FM-Product-Preview-Card-Component/",
+  server: { host: true },
 });
