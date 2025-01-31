04083d1de67179cc31a6ef1a4f57fae405608087
Mark Siazon <markangelosiazon@gmail.com>
2025-01-31 23:47:09 +0800

    🔨 | Update LeftContainer to use imported images

--- src/components/LeftContainer.tsx
+++ src/components/LeftContainer.tsx
@@ -1,3 +1,6 @@
+import productImg_Desktop from "/image-product-desktop.jpg";
+import productImg_Mobile from "/image-product-mobile.jpg";
+
 function LeftContainer() {
   return (
     <section className="__col-1 col-12 col-md-5 col-lg-5 p-0">
@@ -5,12 +8,13 @@ function LeftContainer() {
         <source
           className="__image-2 img-fluid rounded-4"
           media="(max-width:768px)"
-          srcSet="images/image-product-mobile.jpg"
+          srcSet={productImg_Mobile}
         />
         <img
           className="__image-1 img-fluid rounded-4"
-          src="images/image-product-desktop.jpg"
+          src={productImg_Desktop}
           alt="Perfume Product"
+          width={300}
         />
       </picture>
     </section>
