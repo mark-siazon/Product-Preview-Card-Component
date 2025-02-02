7fe60f7b59c5d78af46caaccd3b741de45357353
Mark Siazon <markangelosiazon@gmail.com>
2025-02-03 07:14:24 +0800

    🎨 | Refactor layout and styling in App, RightContainer, and update font imports

--- src/components/RightContainer.tsx
+++ src/components/RightContainer.tsx
@@ -1,21 +1,26 @@
 function RightContainer() {
   return (
-    <section className="__col-2 col ms-3 my-4 my-md-1">
+    <section className="mx-auto md:ms-[30px] mt-[30px] my-md-1 max-w-[275px] font-sans">
       <div className="__hero-header my-0 my-md-3 me-3">
-        <h3 className="fs-5 mb-3">Perfume</h3>
-        <h1 className="fs-1 mb-4">Gabrielle Essence Eau De Parfum</h1>
+        <h3 className="text-xs uppercase mb-3 tracking-[0.3rem] font-sans text-neu-darkgrayishblue ">
+          Perfume
+        </h3>
+        <h1 className="text-3xl mb-4 font-bold font-serif">
+          Gabrielle Essence Eau De Parfum
+        </h1>
       </div>
 
-      <div className="__hero-paragraph me-md-5 my-4">
+      <div className="text-base text-neu-darkgrayishblue">
         <p>
           A floral, solar and voluptuous interpretation composed by Olivier
           Polge, Perfumer-Creator for the House of CHANEL.
         </p>
       </div>
 
-      <div className="__hero-prices col-12 col-md-8 col-lg-6 row mx-auto mx-md-0 my-4">
-        <p className="fs-3 m-auto">$149.99</p>
-        <p className="fs-6 col-5 col-md-3 col-lg-2 my-auto mx-2 text-decoration-line-through text-muted">
+      <div className="flex  my-4 font-serif">
+        {/* __hero-prices */}
+        <p className="text-4xl font-bold text-pri-darkcyan">$149.99</p>
+        <p className="fs-6 col-5 col-md-3 col-lg-2 my-auto mx-2 line-through text-neu-darkgrayishblue text-semibold">
           $169.99
         </p>
       </div>
@@ -23,9 +28,9 @@ function RightContainer() {
       <div className="__hero-btns row">
         <button
           type="button"
-          className="__main-btns col-9 col-md-6 mx-auto btn btn-primary mb-2 mb-md-3 mb-lg-4"
+          className="flex justify-center mx-auto btn btn-primary mb-2 mb-md-3 mb-lg-4 bg-pri-darkcyan py-4 w-full rounded-xl text-neu-white"
         >
-          <img src="images/icon-cart.svg" alt="Add to Cart Icon" />
+          <img src="icon-cart.svg" alt="Add to Cart Icon" />
           <span className="ps-1 text-light">Add to Cart</span>
         </button>
       </div>
