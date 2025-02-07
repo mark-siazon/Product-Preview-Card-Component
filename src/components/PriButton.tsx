498525a0dd1852c9d2f36b44b974d5a134f58465
Mark Siazon <markangelosiazon@gmail.com>
2025-02-07 14:28:06 +0800

    🎨 | Match styles similar to expected output design

--- src/components/PriButton.tsx
+++ src/components/PriButton.tsx
@@ -2,7 +2,7 @@ function PriButton() {
   return (
     <button
       type="button"
-      className="flex gap-x-2 justify-center self-center text-base bg-pri-darkcyan active:brightness-95 hover:brightness-110 transition-all duration-300 py-3 w-full md:max-w-[87.5%] mb-7 rounded-xl text-neu-white"
+      className="flex gap-x-2 justify-center self-center text-base bg-pri-darkcyan active:bg-pri-darkcyan-active  cursor-pointer hover:brightness-115 transition-all duration-500 py-3 w-full md:max-w-[87.5%] mb-7 rounded-xl text-neu-white"
     >
       <img src="icon-cart.svg" className="my-auto" alt="Add to Cart Icon" />
       <span className="text-light">Add to Cart</span>
