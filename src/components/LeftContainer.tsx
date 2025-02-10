d4649f074e517cd235241caadfe3a6ca6f2f9002
Mark Siazon <markangelosiazon@gmail.com>
2025-02-10 13:33:39 +0800

    🔨 | Enhance styling and improve layout matching requirements

--- src/components/LeftContainer.tsx
+++ src/components/LeftContainer.tsx
@@ -8,7 +8,7 @@ function LeftContainer() {
       <picture>
         <source media="(max-width:768px)" srcSet={productImg_Mobile} />
         <img
-          className="img-fluid w-[400px] md:w-[300px] rounded-4 rounded-t-xl md:rounded-r-none md:rounded-l-xl md:h-full"
+          className="img-fluid w-[400px] md:w-[300px] rounded-4 rounded-t-xl md:rounded-r-none md:rounded-l-xl md:h-full brightness-95"
           src={productImg_Desktop}
           alt="Perfume Product"
         />
--- src/components/RightContainer.tsx
+++ src/components/RightContainer.tsx
@@ -2,24 +2,28 @@ import PriButton from "./PriButton";
 
 function RightContainer() {
   return (
-    <section className="mx-auto md:ms-[30px] mt-[30px] max-w-[85%] md:max-w-[290px] font-sans text-neu-darkgrayishblue">
-      <div className="md:max-w-[80%]">
+    <section className="mx-auto md:ms-[30px] mt-[30px] max-w-[85%] md:max-w-[260px] font-sans text-neu-darkgrayishblue">
+      <div className="md:max-w-[85%]">
         <h3 className="text-sm uppercase mb-3 tracking-[0.3rem] ">Perfume</h3>
-        <h1 className="text-4xl mb-4 md:tracking-wide md:leading-9 font-bold font-serif text-neu-verydarkblue">
+        <h1 className="text-4xl mb-4 md:tracking-normal md:leading-9 font-bold font-serif text-neu-verydarkblue">
           Gabrielle Essence Eau De Parfum
         </h1>
       </div>
 
-      <div className="text-base md:max-w-[87.5%] tracking-wide my-6">
+      <div className="text-sm md:max-w-[80%] tracking-wide my-6">
         <p>
           A floral, solar and voluptuous interpretation composed by Olivier
           Polge, Perfumer-Creator for the House of CHANEL.
         </p>
       </div>
 
-      <div className="flex mb-8 font-serif gap-x-4">
-        <p className="text-4xl font-bold text-pri-darkcyan">$149.99</p>
-        <p className="text-base font-medium my-auto line-through">$169.99</p>
+      <div className="flex mb-8 font-serif gap-x-4 md:gap-x-5">
+        <p className="text-4xl md:text-3xl font-bold text-pri-darkcyan">
+          $149.99
+        </p>
+        <p className="text-sm font-sans font-medium my-auto line-through">
+          $169.99
+        </p>
       </div>
 
       <PriButton />
